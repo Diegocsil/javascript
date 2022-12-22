6e5bf085074abbc5d357b2bd93448f9a77668de2
Diego Carvalho da Silva <diegoc.silva4@gmail.com>
2022-12-22 11:30:06 -0300

    aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60
+console.log(`A sua velocidade é ${vel} km/h`)
+
+if(vel > 60){ // Condição simples (quando não tem else)
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segrança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+
+if (pais == 'Brasil'){
+    console.log(`Vivendo em ${pais} você é Brasileiro!`)
+
+}else{
+    console.log(`Vivendo em ${pais} você é estrangeiro!`)
+}
