d8d6c6b62cff44cbb43c564e01a3ed4c45b92702
Diego Carvalho da Silva <diegoc.silva4@gmail.com>
2023-01-06 22:13:46 -0300

    aulas

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+/*
+var c = 1
+
+while (c < 6){
+    console.log('Tudo bem!')
+    c++  //c++ = c+=1
+}
+*/
+
+var n = 2
+
+while (n<=10){
+    console.log(`passo ${n}`)
+    n++
+}
+
+do {
+    n--
+    console.log(`Número ${n}`)
+}while(n>=1)
