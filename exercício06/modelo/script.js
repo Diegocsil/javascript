47a65300eddb16785874fed4628d5c177221acbd
Diego Carvalho da Silva <diegoc.silva4@gmail.com>
2023-01-26 19:47:30 -0300

    Update script.js

--- exercício06/modelo/script.js
+++ exercício06/modelo/script.js
@@ -1,3 +1,4 @@
+/*
 function gerar() {
     let txtnum = document.querySelector('input#txtnum')
     let res = document.querySelector('div#res')
@@ -20,17 +21,38 @@ function gerar() {
         res.innerHTML = `
         <ul> 
         <li>0 x ${n} = ${n0}</li>
-        <li> 1x ${n} = ${n1} </li>
-        <li> 2x ${n} = ${n2} </li>
-        <li> 3x ${n} = ${n3} </li>
-        <li> 4x ${n} = ${n4} </li>
-        <li> 5x ${n} = ${n5} </li>
-        <li> 6x ${n} = ${n6} </li>
-        <li> 7x ${n} = ${n7} </li>
-        <li> 8x ${n} = ${n8} </li>
-        <li> 9x ${n} = ${n9} </li>
+        <li>1 x ${n} = ${n1} </li>
+        <li>2 x ${n} = ${n2} </li>
+        <li>3 x ${n} = ${n3} </li>
+        <li>4 x ${n} = ${n4} </li>
+        <li>5 x ${n} = ${n5} </li>
+        <li>6 x ${n} = ${n6} </li>
+        <li>7 x ${n} = ${n7} </li>
+        <li>8 x ${n} = ${n8} </li>
+        <li>9 x ${n} = ${n9} </li>
         <li>10 x ${n} = ${n10}</li>
         </ul>
         `
+    }
+}
+*/
+
+function gerar() {
+    var num = document.querySelector('input#txtnum')
+    var n = Number(num.value)
+    var tab = document.querySelector('select#seltab')
+
+    if (num.value.length == 0){
+        alert("[ERRO] Digite um numero.")
+    }else{
+        let c = 1
+        tab.innerHTML=''
+        while (c<=10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
     }
 }
