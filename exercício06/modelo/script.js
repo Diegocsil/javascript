d2cf0e4174107477d3af98eb61e016079854ef68
Diego Carvalho da Silva <diegoc.silva4@gmail.com>
2023-01-26 19:56:05 -0300

    Update script.js

--- exercício06/modelo/script.js
+++ exercício06/modelo/script.js
@@ -47,6 +47,13 @@ function gerar() {
     }else{
         let c = 1
         tab.innerHTML=''
+        for(c = 1; c <= 10; c++){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+        /*
         while (c<=10){
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
@@ -54,5 +61,6 @@ function gerar() {
             tab.appendChild(item)
             c++
         }
+        */           
     }
 }
